b244c61db4167832c300cb9bea0144365bd17245
Matthew Fritz <mattf@burbankparanormal.com>
2022-01-22 22:18:48 -0800

    Added autoloading for jQuery to Mix

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,3 +1,8 @@
 let mix = require('laravel-mix');
 
+// ensure we make jQuery available to all modules
+mix.autoload({
+   jquery: ['$', 'window.jQuery']
+});
+
 //mix.js('src/app.js', 'dist').setPublicPath('dist');
